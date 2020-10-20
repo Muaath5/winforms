cfc15d8d4750eddbba3d12ea1a2f383a98a7db24
Muaath5 <60256639+Muaath5@users.noreply.github.com>
2020-10-20 06:56:41 +0300

    To Apply animation in controls

---
+++ src/System.Windows.Forms.Primitives/src/Interop/User32/Interop.AnimateWindow.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class User32
+    {
+        [DllImport(Libraries.User32, ExactSpelling = true)]
+        public static extern bool AnimateWindow(IntPtr hWnd, int flags, int time);
+    }
+}
